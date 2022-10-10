55d9043a27ffb4a0a79fe57bf57931161c655c03
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2022-10-10 18:01:04 +0800

    CAP-55 Ipad non-transactional theme

---
+++ app/design/frontend/X247Commerce/CakeboxIpad/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/X247Commerce/CakeBoxIpad',
+    __DIR__
+);
