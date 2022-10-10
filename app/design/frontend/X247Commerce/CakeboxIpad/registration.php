f81afc1887c44a47ae5970c4c9d15d0b0e59e1f5
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2022-10-10 18:34:49 +0800

    Fix Ipad theme name

--- app/design/frontend/X247Commerce/CakeboxIpad/registration.php
+++ app/design/frontend/X247Commerce/CakeboxIpad/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/X247Commerce/CakeBoxIpad',
+    'frontend/X247Commerce/CakeboxIpad',
     __DIR__
 );
