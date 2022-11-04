4a38fc414a72e5172356787d51331b1e7311cc61
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2022-11-04 16:33:53 +0800

    CAP-97 Assign Magento source to a backend user (admin)

--- app/code/X247Commerce/StoreLocatorSource/Plugin/ModifySourceItems.php
+++ app/code/X247Commerce/StoreLocatorSource/Plugin/ModifySourceItems.php
@@ -40,23 +40,23 @@ class ModifySourceItems
 	    	$roleId = (int) $roleData['role_id'];
 	    	if ($roleId != 1) {
 	    		$userSources = $this->getSourceCodeCollection($userData['user_id']);
-	    	
+	    		    	
 	    		if (isset($result[$product->getId()]['sources']['assigned_sources'])) {
 	    			$items = $result[$product->getId()]['sources']['assigned_sources'];
+
+			    	$list = [];
+			    	$unassign = [];
+			    	foreach ($items as $item) {
+			    		if (in_array($item["source_code"], $userSources)) {
+			    			$list[] = $item;
+			    		} else {
+			    			$unassign[] = $item;
+			    		}
+			    	}
+
+			    	$result[$product->getId()]['sources']['assigned_sources'] = $list;
+			    	$result[$product->getId()]['sources']['unassigned_sources'] = $unassign;
 	    		}
-
-		    	$list = [];
-		    	$unassign = [];
-		    	foreach ($items as $item) {
-		    		if (in_array($item["source_code"], $userSources)) {
-		    			$list[] = $item;
-		    		} else {
-		    			$unassign[] = $item;
-		    		}
-		    	}
-
-		    	$result[$product->getId()]['sources']['assigned_sources'] = $list;
-		    	$result[$product->getId()]['sources']['unassigned_sources'] = $unassign;
 	    	}
     	
 		return $result;
