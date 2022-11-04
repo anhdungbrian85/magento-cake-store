ef54ce2a9d6007cff9fdd4d5c711643b67689c5a
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2022-11-04 15:37:39 +0800

    CAP-118 Validate before place order or add to cart

--- app/code/X247Commerce/Checkout/Observer/Order/ValidateBeforeOrder.php
+++ app/code/X247Commerce/Checkout/Observer/Order/ValidateBeforeOrder.php
@@ -40,46 +40,48 @@ class ValidateBeforeOrder implements ObserverInterface
     public function execute(EventObserver $observer)
     {
         $locationId = $this->customerSession->getStoreLocationId();
-        $searchCriteriaBuilder = $this->searchCriteriaBuilderFactory->create();
-        $searchCriteria = $searchCriteriaBuilder->addFilter('amlocator_store', $locationId, 'in')->create();
-        $sources = $this->sourceRepository->getList($searchCriteria)->getItems();
-        if ($sources) {
-            $sourceCodes = [];
-            foreach ($sources as $source) {
-                $sourceCodes[] =  $source->getSourceCode();
-            }
-        } else {
-            throw new PaymentException(__("Store's source is not available."));
-        }
-        $order = $observer->getEvent()->getOrder();
-        $proSku = [];
-        foreach ($order->getAllItems() as $item) {
-            $proSku[] = $item->getSku();
-        }
-        foreach ($proSku as $sku) {
-            $productQty = $this->productSourceAvailability->getQuantityInformationForProduct($sku);
-            $sourceList = [];
-            foreach ($productQty as $pQty) {
-                if (in_array($pQty['source_code'], $sourceCodes)) {
-                    $sourceList[] = $pQty;
-                }
-            }        
-
-            if ($sourceList) {
-                $inStock = 0;
-                foreach ($sourceList as $qty) {
-                    if ($qty['quantity'] == 0 || !$qty['status']) {
-                        $inStock += 0;
-                    } else {
-                        $inStock += 1;
-                    }
-                }
-
-                if ($inStock == 0) {
-                    throw new PaymentException(__('Some of the products are out of stock.'));
+        if ($locationId) {
+            $searchCriteriaBuilder = $this->searchCriteriaBuilderFactory->create();
+            $searchCriteria = $searchCriteriaBuilder->addFilter('amlocator_store', $locationId, 'in')->create();
+            $sources = $this->sourceRepository->getList($searchCriteria)->getItems();
+            if ($sources) {
+                $sourceCodes = [];
+                foreach ($sources as $source) {
+                    $sourceCodes[] =  $source->getSourceCode();
                 }
             } else {
-                throw new PaymentException(__('Some of the products are not available.'));     
+                throw new PaymentException(__("Store's source is not available."));
+            }
+            $order = $observer->getEvent()->getOrder();
+            $proSku = [];
+            foreach ($order->getAllItems() as $item) {
+                $proSku[] = $item->getSku();
+            }
+            foreach ($proSku as $sku) {
+                $productQty = $this->productSourceAvailability->getQuantityInformationForProduct($sku);
+                $sourceList = [];
+                foreach ($productQty as $pQty) {
+                    if (in_array($pQty['source_code'], $sourceCodes)) {
+                        $sourceList[] = $pQty;
+                    }
+                }        
+
+                if ($sourceList) {
+                    $inStock = 0;
+                    foreach ($sourceList as $qty) {
+                        if ($qty['quantity'] == 0 || !$qty['status']) {
+                            $inStock += 0;
+                        } else {
+                            $inStock += 1;
+                        }
+                    }
+
+                    if ($inStock == 0) {
+                        throw new PaymentException(__('Some of the products are out of stock.'));
+                    }
+                } else {
+                    throw new PaymentException(__('Some of the products are not available.'));     
+                }
             }
         }
         return;
