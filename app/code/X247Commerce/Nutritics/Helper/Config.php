4ece1e2490d92dd73d90b1059a10681abfb90f32
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2022-12-14 11:47:10 +0800

    CAP-107 Nutiritics - integration

---
+++ app/code/X247Commerce/Nutritics/Helper/Config.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace X247Commerce\Nutritics\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+class Config extends AbstractHelper
+{
+    const NUTRITICS_CONFIG_PATH_USERNAME = 'nutritics/api_settings/username';
+    const NUTRITICS_CONFIG_PATH_PASSWORD = 'nutritics/api_settings/password';
+    const NUTRITICS_CONFIG_PATH_API_FUNCTION = 'nutritics/api_settings/function';
+    const NUTRITICS_CONFIG_PATH_USER_ID = 'nutritics/api_settings/user_id';
+    const NUTRITICS_CONFIG_PATH_OBJECT = 'nutritics/api_settings/object';
+    const NUTRITICS_CONFIG_PATH_FILTER = 'nutritics/api_settings/filter';
+    const NUTRITICS_CONFIG_PATH_LIMIT = 'nutritics/api_settings/limit';
+    const NUTRITICS_CONFIG_PATH_ATTRIBUTES = 'nutritics/api_settings/attributes';
+    
+    const NUTRITICS_ENDPOINT = 'www.nutritics.com/api/v1.2';     
+
+    public function __construct(
+        Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+    /**
+     * Get Nutritics Account Username
+     *
+     * @return string|null
+     */
+    public function getNutriticsAccountUsername()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_USERNAME, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get Nutritics Account Password
+     *
+     * @return string|null
+     */
+    public function getNutriticsAccountPassword()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_PASSWORD, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get Nutritics Attributes To Fetch
+     *
+     * @return string
+     */
+    public function getAttributesToFetch()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_ATTRIBUTES, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get Limit To Fetch
+     *
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_LIMIT, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get Filter
+     *
+     * @return int
+     */
+    public function getFilter()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_FILTER, ScopeInterface::SCOPE_STORE);
+    }
+    /**
+     * Get User Id (the ID of the Nutritics user (your developer account can be granted access to multiple Nutritics users) who's objects you would like to work with)
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->scopeConfig->getValue(self::NUTRITICS_CONFIG_PATH_USER_ID, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get Nutritics Base Api EndPoint
+     *
+     * @return string
+     */
+    public function getBaseApiEndPointUrl()
+    {   
+        return self::NUTRITICS_ENDPOINT;
+    }   
+}
---
+++ app/code/X247Commerce/Nutritics/registration.php
@@ -0,0 +1,5 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'X247Commerce_Nutritics', __DIR__);
+
