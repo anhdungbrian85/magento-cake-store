c894e99f476ba1a4977de7ac7d9c66ebfe9acc84
vu247 <vu@247commerce.co.uk>
2023-03-09 16:22:51 +0800

    fix CAP-240: fix unique category

--- app/code/X247Commerce/Catalog/Plugin/Adminhtml/Product/Save.php
+++ app/code/X247Commerce/Catalog/Plugin/Adminhtml/Product/Save.php
@@ -7,6 +7,21 @@ class Save
     {
         $postData = $subject->getRequest()->getPostValue();
         $newCategoryData = $postData['product']['category_show'];
+
+        foreach ($newCategoryData['custom_field'] as $value) {
+            $numPoint = 0;
+            foreach ($newCategoryData['custom_field'] as $key => $value1) {
+                if ($value1['select_field'] == $value['select_field']) {
+                    $numPoint++;
+
+                    if ( $numPoint > 1 ) {
+                        unset($newCategoryData['custom_field'][$key]);
+                        $numPoint--;
+                    }
+                }
+            }
+        }
+
         $postData['product']['category_show_in_popup_crossell'] = json_encode($newCategoryData);
 
         $subject->getRequest()->setPostValue($postData);
