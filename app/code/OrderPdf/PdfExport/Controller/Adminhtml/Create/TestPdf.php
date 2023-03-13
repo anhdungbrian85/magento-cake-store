71ea94dcd961bcf061702c4cd8a410f303806abc
AlbertTran <53705980+Albert-Tran@users.noreply.github.com>
2023-03-13 11:41:22 +0700

    CAP-180: download with application/pdf format

--- app/code/OrderPdf/PdfExport/Controller/Adminhtml/Create/TestPdf.php
+++ app/code/OrderPdf/PdfExport/Controller/Adminhtml/Create/TestPdf.php
@@ -2,19 +2,22 @@
 namespace OrderPdf\PdfExport\Controller\Adminhtml\Create;
 
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Controller\ResultFactory;
 
 class TestPdf extends \Magento\Backend\App\Action
 {
-
     protected $directoryList;
 
+    protected $fileFactory;
     public function __construct(
+        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
         \Magento\Framework\Filesystem\DirectoryList $directoryList,
         Context $context
     ) {
         parent::__construct($context);
         $this->directoryList = $directoryList;
+        $this->fileFactory = $fileFactory;
     }
 
     public function execute()
@@ -25,7 +28,7 @@ class TestPdf extends \Magento\Backend\App\Action
         $logger->info('Start debugging!');
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $mpdf = new \Mpdf\Mpdf([
-            'tempDir' =>  $this->directoryList->getPath('var') . 'log/tmp/mpdf',
+            'tempDir' =>  $this->directoryList->getPath('var') . '/log/tmp/mpdf',
             'margin_left' => 10,
             'margin_right' => 5,
             'margin_top' => 25,
@@ -37,6 +40,13 @@ class TestPdf extends \Magento\Backend\App\Action
         try {
             $mpdf->WriteHTML('Hello World');
             $mpdf->Output('test_order_pdf.pdf', 'I');
+            $fileContent = ['type' => 'string', 'value' => $mpdf->Output('test_order_pdf.pdf', 'S'), 'rm' => true];
+            return $this->fileFactory->create(
+                'invoice.pdf',
+                $fileContent,
+                DirectoryList::VAR_DIR,
+                'application/pdf'
+            );
         } catch (\Exception $e) {
             $logger->info('Has error: ' . $e->getMessage());
             $this->messageManager->addErrorMessage($e->getMessage());
