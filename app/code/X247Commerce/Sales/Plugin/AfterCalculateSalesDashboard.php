12553fbd66c64680811ba795720a7c2f69e95d6f
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2023-03-19 04:05:37 +0800

    CAP-302: calculate sales by admin

---
+++ app/code/X247Commerce/Sales/Plugin/AfterCalculateSalesDashboard.php
@@ -0,0 +1,45 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace X247Commerce\Sales\Plugin;
+
+use X247Commerce\StoreLocatorSource\Helper\User as UserHelper;
+use Magento\Backend\Model\Auth\Session;
+use X247Commerce\StoreLocatorSource\Model\ResourceModel\LocatorSourceResolver;
+
+class AfterCalculateSalesDashboard
+{
+    protected $_adminSession;
+
+    protected $locatorSourceResolver;
+
+    protected $userHelper;
+
+    public function __construct(
+        Session $adminSession,
+        LocatorSourceResolver $locatorSourceResolver,
+        UserHelper $userHelper
+    ) {
+        $this->_adminSession = $adminSession;
+        $this->locatorSourceResolver = $locatorSourceResolver;
+        $this->userHelper = $userHelper;
+    }
+
+    public function afterCalculateSales(
+        \Magento\Reports\Model\ResourceModel\Order\Collection $subject,
+        $result
+    ) {
+        $user = $this->_adminSession->getUser();
+        $isStaffUser = $this->userHelper->isStaffUser($user);
+
+        $amLocatorStoresByUser = $this->locatorSourceResolver->getAmLocatorStoresByUser($user);
+        
+        if ($isStaffUser) {
+            $result->getSelect()
+                    ->joinleft(['slsso' => 'sales_order'], 'main_table.entity_id=slsso.entity_id', [])
+                    ->where('slsso.store_location_id IN (?)', $amLocatorStoresByUser);            
+        }
+        return $result;
+    }
+}
