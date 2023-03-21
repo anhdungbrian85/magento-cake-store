9f99f3025085c3b881e9f0ed57e216c30ecf0ce9
AlbertTran <53705980+Albert-Tran@users.noreply.github.com>
2023-03-21 18:02:14 +0700

    CAP-284: remove customer data of pick up store module

--- app/code/X247Commerce/Checkout/Plugin/CustomerData/LocationData.php
+++ app/code/X247Commerce/Checkout/Plugin/CustomerData/LocationData.php
@@ -3,10 +3,8 @@ namespace X247Commerce\Checkout\Plugin\CustomerData;
 
 class LocationData
 {
-    public function afterGetSectionData(
-    	\Amasty\StorePickupWithLocator\CustomerData\LocationData $subject, 
-    	$result
-    ) {
-        return ['stores' => []];;
+    public function aroundGetSectionData(\Amasty\StorePickupWithLocator\CustomerData\LocationData $subject, callable $proceed)
+    {
+        return [];
     }
 }
