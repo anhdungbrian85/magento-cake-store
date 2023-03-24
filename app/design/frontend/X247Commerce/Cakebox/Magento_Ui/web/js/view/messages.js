f40ac5f98f39514dec36a2b307da6b48756c56df
thong <tphungduc@clever-age.com>
2023-03-24 18:55:21 +0700

    Remove add to cart message on checkout page

---
+++ app/design/frontend/X247Commerce/Cakebox/Magento_Ui/web/js/view/messages.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * @api
+ */
+define([
+    'ko',
+    'jquery',
+    'uiComponent',
+    '../model/messageList',
+    'jquery-ui-modules/effect-blind'
+], function (ko, $, Component, globalMessages) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Magento_Ui/messages',
+            selector: '[data-role=checkout-messages]',
+            isHidden: false,
+            hideTimeout: 5000,
+            hideSpeed: 500,
+            listens: {
+                isHidden: 'onHiddenChange'
+            }
+        },
+
+        /** @inheritdoc */
+        initialize: function (config, messageContainer) {
+            this._super()
+                .initObservable();
+
+            this.messageContainer = messageContainer || config.messageContainer || globalMessages;
+            var self = this;
+
+            // getSuccessMessages
+            self.messageContainer.getErrorMessages().subscribe(function(messages) {
+                console.log(messages)
+                if (messages.length && messages[0].includes('checkout/cart')) {
+                    setTimeout(function() {
+                        self.removeAll()
+                    }, 5000);
+                }
+            });
+            return this;
+        },
+
+        /** @inheritdoc */
+        initObservable: function () {
+            this._super()
+                .observe('isHidden');
+
+            return this;
+        },
+
+        /**
+         * Checks visibility.
+         *
+         * @return {Boolean}
+         */
+        isVisible: function () {
+            return this.isHidden(this.messageContainer.hasMessages());
+        },
+
+        /**
+         * Remove all messages.
+         */
+        removeAll: function () {
+            this.messageContainer.clear();
+        },
+
+        /**
+         * @param {Boolean} isHidden
+         */
+        onHiddenChange: function (isHidden) {
+            // Hide message block if needed
+            if (isHidden) {
+                setTimeout(function () {
+                    $(this.selector).hide('blind', {}, this.hideSpeed);
+                }.bind(this), this.hideTimeout);
+            }
+        }
+    });
+});
