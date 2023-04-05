ef38c0de339fe4809f8f221df9212d1ced140265
HTCSoft Dev <dev@vmi933993.contaboserver.net>
2023-04-05 12:09:30 +0800

    CAP-325 Restrict day for asda store

--- app/code/X247Commerce/Checkout/view/frontend/web/js/view/pickup/pickup-date-mixin.js
+++ app/code/X247Commerce/Checkout/view/frontend/web/js/view/pickup/pickup-date-mixin.js
@@ -28,7 +28,6 @@ define([
         getFirstPickupDate: function (store) {
             var minPickupDate = this.minPickupDateTime.asDateTimeObject,
                 index;
-
             if (!store.schedule_id) {
                 this.storeScheduleSelected(false);
 
@@ -61,7 +60,68 @@ define([
 
             return minPickupDate;
         },
+        /**
+         * Check if date is valid
+         * Method returns [false, ''] if date is restricted
+         * Method returns [true, ''] if date is NOT restricted
+         *
+         * @param {Date||String} date
+         * @return {[boolean, string]}
+         */
+        restrictDates: function (date) {
+            var selectedStore = this.selectedStore,
+                storeDateTime,
+                isToday,
+                minPickupDateWithoutTime,
+                dateWithoutTime,
+                currentDayName,
+                daySchedule,
+                scheduleArray;
 
+            if (!selectedStore) {
+                return [false, ''];
+            }
+
+            storeDateTime = this.currentStoreDateTime.asDateTimeObject;
+            isToday = this.isDateIsStoreToday(date, storeDateTime);
+
+            minPickupDateWithoutTime = new Date(
+                this.minPickupDateTime.asDateTimeObject.getUTCFullYear(),
+                this.minPickupDateTime.asDateTimeObject.getUTCMonth(),
+                this.minPickupDateTime.asDateTimeObject.getUTCDate()
+            );
+
+            dateWithoutTime = new Date(
+                date.getFullYear(),
+                date.getMonth(),
+                date.getDate()
+            );
+
+            if (dateWithoutTime < minPickupDateWithoutTime) {
+                return [false, ''];
+            }
+
+            if (selectedStore.schedule_id) {
+                scheduleArray = pickupDataResolver.getScheduleByScheduleId(selectedStore.schedule_id);
+                currentDayName = this.weekDays[date.getDay()];
+                daySchedule = scheduleArray[currentDayName];
+
+                // check current day status in Store Schedule object
+                if (!+daySchedule[currentDayName + '_status']) {
+                    return [false, ''];
+                }
+            }
+
+            if (isToday && !this._isSameDayAllowed(daySchedule)) {
+                return [false, ''];
+            }
+
+            if (isToday && locationContext.isAsda()) {
+                return [false, ''];
+            }
+
+            return [true, ''];
+        },
     };
 
     return function (target) {
