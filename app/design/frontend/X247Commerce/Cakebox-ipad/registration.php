96705c651585a24700d804f3a8ed4435aa9a5935
htcdev <testmage321@gmail.com>
2023-05-10 17:09:26 +0700

    CAP-392 create theme design ipad

---
+++ app/design/frontend/X247Commerce/Cakebox-ipad/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/X247Commerce/Cakebox-ipad',
+    __DIR__
+);
