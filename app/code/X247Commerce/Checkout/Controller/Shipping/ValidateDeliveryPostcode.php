8d22d288d6d543e190da532239ae3409bd17e032
thong <tphungduc@clever-age.com>
2023-05-11 14:41:59 +0700

    CAP-398 Add error message when delivery postcode is not valid

---
+++ app/code/X247Commerce/Checkout/Controller/Shipping/ValidateDeliveryPostcode.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace X247Commerce\Checkout\Controller\Shipping;
+
+use Magento\Checkout\Controller\Checkout;
+use Magento\Checkout\Model\Cart as CustomerCart;
+use Magento\Checkout\Model\Session;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Amasty\Storelocator\Model\LocationFactory;
+use Amasty\Storelocator\Model\ResourceModel\Location\Collection as LocationCollection;
+use Amasty\Storelocator\Model\ResourceModel\Location\CollectionFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Store\Model\StoreManagerInterface;
+use X247Commerce\Checkout\Api\StoreLocationContextInterface;
+use X247Commerce\StoreLocator\Helper\DeliveryArea;
+
+class ValidateDeliveryPostcode extends Action
+{
+
+    protected StoreLocationContextInterface $storeLocationContext;
+    protected LocationFactory $locationFactory;
+    protected DeliveryArea $deliveryAreaHelper;
+    protected JsonFactory $resultJsonFactory;
+
+    public function __construct(
+        Context $context,
+        StoreLocationContextInterface $storeLocationContext,
+        LocationFactory $locationFactory,
+        DeliveryArea $deliveryAreaHelper,
+        JsonFactory $resultJsonFactory
+    ) {
+        parent::__construct($context);
+        $this->storeLocationContext = $storeLocationContext;
+        $this->locationFactory = $locationFactory;
+        $this->deliveryAreaHelper = $deliveryAreaHelper;
+        $this->resultJsonFactory = $resultJsonFactory;
+    }
+
+
+    public function execute()
+    {
+        $resultJson = $this->resultJsonFactory->create();
+        $postcode = $this->getRequest()->getParam('postcode');
+        $storeLocationId = $this->getRequest()->getParam('store_location_id');
+        if (!$postcode || !$storeLocationId) {
+            return $resultJson->setData(
+                ['valid' => -1]
+            );
+        }
+
+        return $resultJson->setData(
+            ['valid' => $this->deliveryAreaHelper->checkPostcodeWithStore($postcode, $storeLocationId)]
+        );
+    }
+}
