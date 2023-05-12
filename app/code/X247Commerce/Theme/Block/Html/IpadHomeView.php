0c492a2d5ff3ba5e7994644b8a4a7bef7a792054
htcdev <testmage321@gmail.com>
2023-05-12 18:59:46 +0700

    CAP-392 create home page ipad design

---
+++ app/code/X247Commerce/Theme/Block/Html/IpadHomeView.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace X247Commerce\Theme\Block\Html;
+
+use \Magento\Catalog\Block\Category\View;
+
+class IpadHomeView extends View implements \Magento\Framework\DataObject\IdentityInterface
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * Catalog layer
+     *
+     * @var \Magento\Catalog\Model\Layer
+     */
+    protected $_catalogLayer;
+
+    /**
+     * @var \Magento\Catalog\Helper\Category
+     */
+    protected $_categoryHelper;
+
+    protected $categoryCollectionFactory;
+
+    protected $categoryRepository;
+
+    protected $_storeManager;
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Catalog\Model\Layer\Resolver $layerResolver
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Catalog\Helper\Category $categoryHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\Layer\Resolver $layerResolver,
+        \Magento\Framework\Registry $registry,
+        \Magento\Catalog\Helper\Category $categoryHelper,
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
+        \Magento\Catalog\Model\CategoryRepository $categoryRepository,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        array $data = []
+    ) {
+        parent::__construct($context, $layerResolver, $registry, $categoryHelper, $data);
+        $this->_categoryHelper = $categoryHelper;
+        $this->_catalogLayer = $layerResolver->get();
+        $this->_coreRegistry = $registry;
+        $this->categoryRepository = $categoryRepository;
+        $this->_storeManager = $storeManager;
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+    }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $category = $this->getCurrentCategory();
+        if ($breadcrumbsBlock = $this->getLayout()->getBlock('breadcrumbs')) {
+            $breadcrumbsBlock->addCrumb(
+                'category'.$category->getId(),
+                [
+                    'label' => __($category->getName()),
+                ]
+            );
+        }
+        return $this;
+    }
+
+    public function getCurrentCategory(){
+        $categoryId = 74;
+        return $this->categoryRepository->get($categoryId, $this->_storeManager->getStore()->getId());
+    }
+}
