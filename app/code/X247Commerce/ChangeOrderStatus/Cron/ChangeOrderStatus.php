814233f1293c99cdc4a70a70a4fd3674c9de32bd
thong <tphungduc@clever-age.com>
2023-06-06 11:41:46 +0700

    X247Commerce_ChangeOrderStatus change logic

--- app/code/X247Commerce/ChangeOrderStatus/Cron/ChangeOrderStatus.php
+++ app/code/X247Commerce/ChangeOrderStatus/Cron/ChangeOrderStatus.php
@@ -31,66 +31,83 @@ class ChangeOrderStatus
 
 	public function execute()
 	{
-
 		$statuses = array( 'pending', 'processing' );
 		$collection = $this->orderCollectionFactory->create()
 			->addFieldToSelect('*')
-			->addFieldToFilter('status', ['in' => $statuses] );
-		$collection->getSelect()->limit(50);
+			->addFieldToFilter('status', ['in' => $statuses] )
+            ->addFieldToFilter('auto_complete_flag', ['neq' => 1]);
+
+        $collection->getSelect()->joinLeft(
+            ['aam' => 'amasty_amcheckout_delivery'],
+            'aam.order_id = main_table.entity_id',
+            ['delivery_date' => 'date']
+        );
+
+        $collection->getSelect()->joinLeft(
+            ['aso' => 'amasty_storepickup_order'],
+            'aso.order_id = main_table.entity_id',
+            ['pickup_date' => 'date']
+        );
+        $collection->getSelect()->limit(50);
+
 		$dayToChangeOrder = (int) $this->changeOrderStatusHelper->getNumberDayChangeStatus();
-		
+
 		foreach ($collection as $order) {
-			$orderData = $this->amsOrderRepository->getByOrderId($order->getId());
-			$createdAt = $order->getCreatedAt();
-			$date = $orderData->getDate();
-			
-			if ( $date ) {
-				$today = date('Y-m-d');
-				$today = strtotime($today);
-				$converted = strtotime($date);
-				
-				if ( ( $today - $converted ) > 0 && ($today- $converted)/86400 >= $dayToChangeOrder ) {
-					$order->setStatus(\Magento\Sales\Model\Order::STATE_COMPLETE);
-					$order->save();
 
-					if($order->canInvoice()) {
-						$invoice = $this->invoiceService->prepareInvoice($order);
-						$invoice->register();
-						$invoice->save();
-						$transactionSave = $this->transaction->addObject(
-							$invoice
-						)->addObject(
-							$invoice->getOrder()
-						);
-						$transactionSave->save();
-					}
+            $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/autocomplete-order.log');
+            $logger = new \Zend_Log();
+            $logger->addWriter($writer);
 
-					if ($order->canShip()) {
-						$orderShipment = $this->convertOrder->toShipment($order);
-						foreach ($order->getAllItems() as $item) {
-							if (!$item->getQtyToShip() || $item->getIsVirtual()) {
-								continue;
-							}
-							$qty = $item->getQtyToShip();
-							$shipmentItem = $this->convertOrder->itemToShipmentItem($item)->setQty($qty);
-							$orderShipment->addItem($shipmentItem);
-						}
-						$orderShipment->register();
-						$orderShipment->getOrder()->setIsInProcess(true);
-						try {
-							//$orderShipment->save();
-							$orderShipment->getOrder()->save();
+			$date = $order->getData('delivery_date');
+            if (empty($date)) {
+                $date = $order->getData('pickup_date');
+            }
+			if (!empty($date)) {
+                $today = date('Y-m-d');
+                $today = strtotime($today);
+                $converted = strtotime($date);
+                try {
+                    if ( ( $today - $converted ) > 0 && ($today- $converted)/86400 >= $dayToChangeOrder ) {
+                        if($order->canInvoice()) {
+                            $invoice = $this->invoiceService->prepareInvoice($order);
+                            $invoice->register();
+                            $invoice->save();
+                            $transactionSave = $this->transaction->addObject(
+                                $invoice
+                            )->addObject(
+                                $invoice->getOrder()
+                            );
+                            $transactionSave->save();
+                        }
 
-						} catch (\Exception $e) {
-							throw new \Magento\Framework\Exception\LocalizedException(
-							__($e->getMessage())
-							);
-						}
-					}
+                        if ($order->canShip()) {
+                            $orderShipment = $this->convertOrder->toShipment($order);
+                            foreach ($order->getAllItems() as $item) {
+                                if (!$item->getQtyToShip() || $item->getIsVirtual()) {
+                                    continue;
+                                }
+                                $qty = $item->getQtyToShip();
+                                $shipmentItem = $this->convertOrder->itemToShipmentItem($item)->setQty($qty);
+                                $orderShipment->addItem($shipmentItem);
+                            }
+                            $orderShipment->register();
+                            $orderShipment->getOrder()->setIsInProcess(true);
 
-				}
+                            $orderShipment->getOrder()->save();
+
+                            $order->setStatus('complete');
+                            $order->setState('complete');
+                        }
+
+                        $order->setData('auto_complete_flag', 1);
+                        $order->save();
+                    }
+                }   catch (\Exception $e) {
+                    $logger->info('cannot complete order: '.$order->getId(). ' - '.$e->getMessage());
+                    $order->setData('auto_complete_flag', 1);
+                    $order->save();
+                }
 			}
 		}
-
 	}
 }
