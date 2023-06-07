69b58bedd21e512c759187d787cf97c28c12d7b1
AlbertTran <53705980+Albert-Tran@users.noreply.github.com>
2023-06-07 10:15:55 +0700

    CAP-448: add validate size in order pdf

--- app/code/OrderPdf/PdfExport/Helper/Data.php
+++ app/code/OrderPdf/PdfExport/Helper/Data.php
@@ -158,9 +158,13 @@ class Data extends AbstractHelper
                             $base = 'CA';
                             break;
                     }
-                    $size = str_replace('" serves ', "x", $size_serving); // 10 6
-                    $size = str_replace('Cupcakes serves ', "x ", $size); // 10 6
-//                    $size = str_replace('"'," ",substr($size_serving, 0, 3)); // 10 6
+                    if (strpos($size_serving, '" serves ')) {
+                        $size = str_replace('" serves ', "x", $size_serving); // 10 6
+                    } else if(strpos($size_serving, 'Cupcakes serves')) {
+                        $size = str_replace('Cupcakes serves ', " x ", $size_serving); // 10 6
+                    } else {
+                        $size = str_replace('"'," ",substr($size_serving, 0, 3)); // 10 6
+                    }
                     $colour = $product->getAttributeText('color') ? $product->getAttributeText('color') : "";
                     $colorOption = $product->getResource()->getAttribute('color')->getSource()->getSpecificOptions($product->getData('color'));
                     $colourOptionId = 0;
