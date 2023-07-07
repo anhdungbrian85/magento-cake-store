a34685dd061c886517ed8cbbcf545b4613a956ed
Cong Vu <vu@247commerce.co.uk>
2023-07-07 18:45:56 +0700

    CAP-468, CAP-469

---
+++ app/code/X247Commerce/Checkout/Plugin/Checkout/Block/Checkout/AttributeMerger/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace X247Commerce\Checkout\Plugin\Checkout\Block\Checkout\AttributeMerger;
+
+class Plugin
+{
+  public function afterMerge(\Magento\Checkout\Block\Checkout\AttributeMerger $subject, $result)
+  {
+    if (array_key_exists('street', $result)) {
+      $result['street']['children'][0]['placeholder'] = __('Type first line of address and select from dropdown');
+      $result['street']['children'][1]['placeholder'] = __('Type first line of address and select from dropdown');
+    }
+
+    return $result;
+  }
+}
