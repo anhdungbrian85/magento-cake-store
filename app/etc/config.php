a668d62956aa2f39b74ae374bf0abe6f84af6f9e
thong <tphungduc@clever-age.com>
2023-07-19 11:37:21 +0700

    CAP-476 Add validate delivery, pickup date/time before placing order

--- app/etc/config.php
+++ app/etc/config.php
@@ -520,6 +520,7 @@ return [
         'X247Commerce_PopupAddtoCart' => 1,
         'X247Commerce_Products' => 1,
         'X247Commerce_ReadMoreDescriptionCategory' => 0,
+        'X247Commerce_RemoveOrderImages' => 1,
         'X247Commerce_StoreLocatorSource' => 1,
         'X247Commerce_ShopBy' => 1,
         'X247Commerce_Staging' => 1,
