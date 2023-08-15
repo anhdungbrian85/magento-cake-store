15afb677a2211d1a399749612f88f668d2372c8f
ThongPhung <phung.thong@247commerce.co.uk>
2023-08-15 18:50:23 +0700

    Revert "CAP-488 log urlBuilder delivery popup"
    This reverts commit dc3c42acd8b442751727f684e703c92b49c26293.

--- app/code/X247Commerce/DeliveryPopUp/view/frontend/web/js/deliverypopup.js
+++ app/code/X247Commerce/DeliveryPopUp/view/frontend/web/js/deliverypopup.js
@@ -19,7 +19,6 @@ define([
                     class: '',
                 }],
                 closed: function() {
-                    console.log('urlBuilder_closed', urlBuilder)
                     $.ajax({
                         url: urlBuilder.build('deliverypopup/index/close'),
                         method: "POST",
@@ -36,7 +35,6 @@ define([
             var popup = modal(options, $('#custom-delivery-popup-modal'));
 
             var currentUrl = window.location.href;
-            console.log('urlBuilder_deliverypopup', urlBuilder)
             $.ajax({
                 url: urlBuilder.build('deliverypopup'),
                 method: "POST",
