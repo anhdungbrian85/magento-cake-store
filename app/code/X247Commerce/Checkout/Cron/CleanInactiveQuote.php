39e72081f94f2587079b25cde7e5d4f8bc3d4620
ThongPhung <phung.thong@247commerce.co.uk>
2023-09-15 14:32:14 +0700

    CAP-504 clean inactive quote

---
+++ app/code/X247Commerce/Checkout/Cron/CleanInactiveQuote.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace X247Commerce\Checkout\Cron;
+
+use X247Commerce\Checkout\Console\CleanInactiveQuote as CleanInactiveQuoteCommand;
+
+class CleanInactiveQuote
+{
+    protected CleanInactiveQuoteCommand $cleanActiveQuoteCommand;
+
+    public function __construct(
+    	CleanInactiveQuoteCommand $cleanActiveQuoteCommand
+    ) {
+		$this->cleanActiveQuoteCommand = $cleanActiveQuoteCommand;
+    }
+
+    /**
+     * @return void
+     * */
+    public function execute()
+    {
+        $this->cleanActiveQuoteCommand->execute();
+    }
+
+}
