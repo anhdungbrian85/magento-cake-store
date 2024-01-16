e542b69a20bba6d4fdfe0c71ac2a453d5f3188e1
ThongPhung <phung.thong@247commerce.co.uk>
2024-01-16 02:47:55 +0000

    Revert "Revert "SPTCAK-44 add lead_delivery in collection holiday""
    This reverts commit 7e6f736b8e5b6d17618b353d00ad1815c6a1f8e0.

--- app/design/frontend/X247Commerce/Cakebox/Amasty_StorePickupWithLocator/web/js/view/pickup/pickup-time.js
+++ app/design/frontend/X247Commerce/Cakebox/Amasty_StorePickupWithLocator/web/js/view/pickup/pickup-time.js
@@ -188,14 +188,13 @@ define([
             firstScheduledTimeUnix = intervals[0].fromInUnix;
             
             filteredIntervals = intervals.filter(function (item) {
-
                 if (!$.isEmptyObject(holidayTime)) {
                     let compare = (holidayTime.openInUnix < item.fromInUnix && holidayTime.closeInUnix > item.fromInUnix);
                     if (this.isTodaySelected) {
                         if (currentStoreTime > firstScheduledTimeUnix) {
                             return compare && (item.fromInUnix > (currentStoreTime + parseInt(locationContext.leadDeliveryTime())*3600));
                         } else {
-                            return compare && (item.fromInUnix >= (firstScheduledTimeUnix + parseInt(locationContext.leadDeliveryTime())*3600));
+                            return compare && (item.fromInUnix >= (holidayTime.openInUnix + parseInt(locationContext.leadDeliveryTime())*3600));
                         }
                     } else {
                         return compare;
