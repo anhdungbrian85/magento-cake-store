d412405b8f3a935fb7a942da65bc559688c7897b
ThongPhung <phung.thong@247commerce.co.uk>
2024-01-16 02:48:00 +0000

    Revert "Revert "Ading condition and logic to check validation for bundle product""
    This reverts commit 978da96d45d032428c51ccb71aa0fc61585e7fe6.

--- app/code/X247Commerce/StoreLocator/Observer/Cart/ValidateOutOfStockStatus.php
+++ app/code/X247Commerce/StoreLocator/Observer/Cart/ValidateOutOfStockStatus.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace X247Commerce\StoreLocator\Observer\Cart;
 
 use Magento\Framework\Event\Observer;
@@ -15,14 +14,18 @@ class ValidateOutOfStockStatus implements ObserverInterface
 
     protected $locatorSourceResolver;
 
+    protected $_productloader;
+
     public function __construct(
         \Magento\Checkout\Model\Session $checkoutSession,
         \X247Commerce\Checkout\Api\StoreLocationContextInterface $locationContext,
-        LocatorSourceResolver $locatorSourceResolver
+        LocatorSourceResolver $locatorSourceResolver,
+        \Magento\Catalog\Model\ProductFactory $_productloader
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->locationContext = $locationContext;
         $this->locatorSourceResolver = $locatorSourceResolver;
+        $this->_productloader = $_productloader;
     }
 
     /**
@@ -34,6 +37,7 @@ class ValidateOutOfStockStatus implements ObserverInterface
         $logger = new \Zend_Log();
         $logger->addWriter($writer);
         $logger->info('Starting debug');
+        $currentProduct = '';
         $selectedLocationId = $this->locationContext->getStoreLocationId();
         $quote = $this->checkoutSession->getQuote();
         $logger->info('Selected Location Id: ' . $selectedLocationId );
@@ -42,17 +46,62 @@ class ValidateOutOfStockStatus implements ObserverInterface
         $logger->info('Current Produc Sku: ' . $currentProduct->getSku() );
         $logger->info('Product Type: ' . $currentProduct->getTypeId());
         $logger->info('Quote Item Id: ' . $quote->getId());
+        /** Bundle logic SPTCAK-46 */
+        if($currentProduct->getTypeId() == 'bundle'){
+            $logger->info('Starting Bundle');
+            $logger->info('store location id:'. $selectedLocationId);
+            $productIds = [];
+           // $logger->info('Bundle Item Id: ' . json_encode($currentProduct->getData(), true) );
+            foreach($quote->getAllVisibleItems() as $bundleitem) {
+                //$logger->info('All Item data: ' . json_encode($bundleitem->getData(), true) );
+                //$logger->info('inside foreach');
+                $logger->info('Entity Id:'. $currentProduct->getData('entity_id'));
+                $logger->info('Product Id:'. $bundleitem->getData('product_id'));
+                if($currentProduct->getData('entity_id') == $bundleitem->getData('product_id')){
+                    $itemOptions = $bundleitem->getdata('qty_options');
 
-        if (!$this->locatorSourceResolver->validateOutOfStockStatusOfProduct($selectedLocationId, $currentProduct->getSku())) {
-            $logger->info('Error current product: ' . $currentProduct->getSku() );
-            throw new \Magento\Framework\Exception\LocalizedException(__('The current product is out of stock on this location.'));
-        }
-        foreach($quote->getAllVisibleItems() as $item) {
-            if (!$this->locatorSourceResolver->checkProductAvailableInStore($selectedLocationId, $item)) {
-                $logger->info('Error sku: ' . $item->getSku() );
+                    foreach ($itemOptions as $key => $value) {
+                        $productIds[] = $key;
+                    }
+                    //$logger->info('Parent Item Id: ' . print_r($productIds, true) );
+                }
+            }
+            //$logger->info('Parent item id before foreach: ' . print_r($productIds, true) );
+             foreach($productIds as $childId){
+                $logger->info('child item id: ' . print_r($childId, true) );
+                $childData = $this->getLoadProduct($childId);
+                    if (!$this->locatorSourceResolver->validateOutOfStockStatusOfProduct($selectedLocationId, $childData->getSku())) {
+                        $logger->info('Error current product: ' . $childData->getSku() );
+                        throw new \Magento\Framework\Exception\LocalizedException(__('The current product is out of stock on this location.'));
+                    }
+
+                    if (!$this->locatorSourceResolver->checkProductAvailableInStore($selectedLocationId, $childData)) {
+                        $logger->info('Error sku: ' . $childData->getSku() );
+                        throw new \Magento\Framework\Exception\LocalizedException(__('The product is out of stock on this location.'));
+                    }
+             }
+         }else{
+            //$logger->info('Starting Non-bundle');
+            //$logger->info('store location id:'. $selectedLocationId);
+            //$logger->info('Current product id: '. $currentProduct->getSku());
+            if (!$this->locatorSourceResolver->validateOutOfStockStatusOfProduct($selectedLocationId, $currentProduct->getSku())) {
+                $logger->info('Error current product: ' . $currentProduct->getSku() );
+                throw new \Magento\Framework\Exception\LocalizedException(__('The current product is out of stock on this location.'));
+            }
+            $productData = $this->getLoadProduct($currentProduct->getId());
+           // $logger->info('Parent Item Id: ' . json_encode($currentProduct->getData(), true) );
+            if (!$this->locatorSourceResolver->checkProductAvailableInStore($selectedLocationId, $productData)) {
+                $logger->info('Error sku: ' . $currentProduct->getId() );
                 throw new \Magento\Framework\Exception\LocalizedException(__('The product is out of stock on this location.'));
             }
-        }
+         }
+
+
         $logger->info('Ending debug');
+    }
+
+    public function getLoadProduct($id)
+    {
+        return $this->_productloader->create()->load($id);
     }
 }
