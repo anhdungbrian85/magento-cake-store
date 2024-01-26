7869e0461ef824bdf0c5660612cbfdb19385fc5d
ThongPhung <phung.thong@247commerce.co.uk>
2024-01-26 09:14:55 +0000

    SPTCAK-61 fix Bundle - Lead time issue

--- app/code/X247Commerce/Checkout/Plugin/CustomerData/DefaultItem.php
+++ app/code/X247Commerce/Checkout/Plugin/CustomerData/DefaultItem.php
@@ -3,7 +3,9 @@ namespace X247Commerce\Checkout\Plugin\CustomerData;
 
 class DefaultItem
 {
-    public function afterGetItemData(\Magento\Checkout\CustomerData\DefaultItem $subject, $result, \Magento\Quote\Model\Quote\Item $item)
+    public function afterGetItemData(
+        \Magento\Checkout\CustomerData\DefaultItem $subject,
+        $result, \Magento\Quote\Model\Quote\Item $item)
     {
         if ($item->getProductType() == 'simple') {
             $result['lead_delivery'] = $item->getProduct()->getData('lead_delivery');
@@ -12,13 +14,18 @@ class DefaultItem
             $childItem = null;
             foreach ($quote->getAllItems() as $quoteItem) {
                 if ($quoteItem->getParentItemId() == $item->getId()) {
-                    $childItem = $quoteItem;  
-                    break;
+                    if (!isset($maxLeadDelivery)) {
+                        $maxLeadDelivery = $quoteItem->getProduct()->getData('lead_delivery');
+                    }   else {
+                        $maxLeadDelivery = $maxLeadDelivery > $quoteItem->getProduct()->getData('lead_delivery') ?
+                            $maxLeadDelivery : $quoteItem->getProduct()->getData('lead_delivery');
+                    }
+
                 }
             }
-            $result['lead_delivery'] = $childItem->getProduct()->getData('lead_delivery');
+
         }
-        
+        $result['lead_delivery'] = $maxLeadDelivery;
         return $result;
     }
 }
